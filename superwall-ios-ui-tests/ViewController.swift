481caa518334dd5c26eba88640aa973e26a7361e
Bryan Dubno <bryan@superwall.com>
2023-01-24 15:37:36 -0800

    Initial Commit

---
+++ superwall-ios-ui-tests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  superwall-ios-ui-tests
+//
+//  Created by Bryan Dubno on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
