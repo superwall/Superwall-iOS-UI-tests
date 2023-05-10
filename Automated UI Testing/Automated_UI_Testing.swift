858ee25fdda0344b894c5b41260c2894bd3c5669
Bryan Dubno <bryan@superwall.com>
2023-05-10 10:19:17 -0700

    Add back valid timeout

--- Automated UI Testing/Automated_UI_Testing.swift
+++ Automated UI Testing/Automated_UI_Testing.swift
@@ -79,7 +79,7 @@ class Automated_UI_Testing: XCTestCase {
   func performSDKTest(number: Int) async throws {
     // Handle 5 minute timeout
     let timeoutTask = Task {
-      await Task.sleep(timeInterval: 30)
+      await Task.sleep(timeInterval: 300)
       guard Task.isCancelled == false else { return }
       #warning("log failure better")
       XCTFail("Timeout for test #\(number)")
