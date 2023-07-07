bb89e9428fd3581159727ef23e6d181466e7faa0
Bryan Dubno <bryan@superwall.com>
2023-07-07 13:28:15 -0700

    Update screens for Xcode Cloud

--- Automated UI Testing/Automated_UI_Testing.swift
+++ Automated UI Testing/Automated_UI_Testing.swift
@@ -37,10 +37,7 @@ class Automated_UI_Testing: XCTestCase {
 
       case .assert(let testName, let precision, let captureArea):
         // If Xcode 14.1/14.2 bug ever gets fixed, use `simctl` to set a consistent status bar instead (https://www.jessesquires.com/blog/2022/12/14/simctrl-status_bar-broken/)
-//        let image = captureArea.image(from: app.screenshot().pngRepresentation)
-//        let image = app.screenshot().image
-//        let image = UIImage(data: imageData)
-        let image = UIImage(data: app.screenshot().pngRepresentation)!
+        let image = captureArea.image(from: app.screenshot().image)
         ciAssertSnapshot(matching: image, as: .image(precision: precision), testName: testName)
         Communicator.shared.completed(action: action)
 
